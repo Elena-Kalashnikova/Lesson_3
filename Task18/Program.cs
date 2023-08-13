d3d94c681878bc5ab5a68e4526f1885069f6c158
Елена <lenna@gmail.com>
2023-08-13 20:06:02 +0300

    Added program in Tasl18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+Console.Write("N четверти: ");
+string number = Console.ReadLine();
+string quarter = GetQuarterNumber(number);
+string result = quarter != null?
+ $"Указанной номер четверти соответсвует диапозону -> {quarter}": 
+ "Введите коректный номер четверти";
+Console.WriteLine(result);
+
+
+string GetQuarterNumber(string num)
+{ 
+    if (num == "1") return "x > 0, y > 0";
+    if (num == "2") return "x < 0, y > 0";
+    if (num == "3") return "x < 0, y < 0";
+    if ( num == "4") return "x > 0, y < 0";
+    return null;
+}
+
+
