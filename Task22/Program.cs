f13503c630a700bac7f51240be394a205a1ea322
Елена <lenna@gmail.com>
2023-08-13 21:05:51 +0300

    Added program in Tasl22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else
+{
+   TableSquare(number);
+   
+ 
+}
+
+void TableSquare(int num){
+    int count = 1;
+     while (count <= num)
+     {
+        Console.WriteLine($"{count} {count * count}" );
+        count++;
+        
+     }
+}
+
